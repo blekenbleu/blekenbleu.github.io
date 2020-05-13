faab9ffd960355ff8eea1e35980e3e14df23217c
Oristo Po <ormpoa@gmail.com>
2020-05-13 12:11:13 -0400

    color coding new usa facts cases

---
+++ covid/diff_covid.p
@@ -0,0 +1,27 @@
+unset border
+set term wxt 0 enhanced size 1400,800
+set style data boxes
+set style fill solid 1.0 border -1
+set linetype 1 lc rgb "gray" lw 0 pt 1
+set linetype 2 lc rgb "yellow" lw 0 pt 1
+set linetype 3 lc rgb "orange" lw 0 pt 1
+set linetype 4 lc rgb "#FF4444" lw 0 pt 1
+set linetype 5 lc rgb "#AA0000" lw 0 pt 1
+set xtics border rotate 90 offset 0,graph 0 nomirror
+set logscale y
+unset grid
+unset y2tics
+set ytics nomirror
+set grid ytics front
+set ylabel title
+set yrange [1:5000]
+f(a,b) = (a > b) ? a : 0
+plot 'data.txt' using 2 t '> 20 days'
+replot 'data.txt' using ($2-$5) t '6-20 days'
+replot 'data.txt' using ($2-$4) t '3-6 days'
+# differently color-code increasing 3 day cases
+replot 'data.txt' using ($2-$3) t '1-3 days'
+replot 'data.txt' using (f($2-$3,$3-$4)):xticlabels(6) t 'increasing 1-3 days'
+unset output
+set term png size 1400,800
+replot
---
+++ covid/usfact.sh
@@ -0,0 +1,92 @@
+#! /bin/bash 
+# generate a two frame covid animated gif using USA fact csv
+source cfg.sh
+if [ -n "$oops" ] ; then
+  echo animate.sh:  missing $oops
+  return 1
+fi
+
+if [ ! -r factFIPS.csv ] ; then
+  echo source newFIPS.sh factFIPS.csv
+  return 2
+fi
+
+fco=covid_confirmed_usafacts.csv
+curl "https://usafactsstatic.blob.core.windows.net/public/data/covid-19/$fco" > $CTMP/$fco
+fco=$CTMP/$fco
+
+count()
+{
+  a0=$#
+  echo $a0 items in $fco
+  let a3=$a0-3
+  let a6=$a0-6
+  let a20=$a0-20
+  now="${!a0}"
+  now3="${!a3}"
+  now6="${!a6}"
+  now20="${!a20}"
+  echo "#index	$now	$now3	$now6	$now20	Location" > $CTMP/data.txt
+}
+
+# rounded values per 100K
+rnd100k()
+{
+  let v=100000*$1
+  let r=$Pop/2
+  let v=$v+$r
+  let v=$v/$Pop
+  echo $v
+}
+  
+county()
+{
+# approx most recent 20 days are contagious
+  let d20=${!a20}
+  let d6=${!a6}
+  let d3=${!a3}
+  let d0=${!a0}
+  d20=`rnd100k $d20`
+  d6=`rnd100k $d6`
+  d3=`rnd100k $d3`
+  d0=`rnd100k $d0`
+# do not plot identical newer over older
+  if [ $d3 -eq -$d6 ] ; then
+    d3=$d0
+  fi
+  if [ $d6 -eq -$d20 ] ; then
+    d6=$d3
+  fi
+  echo "$i	$d0	$d3	$d6	$d20	$loc" >> data.txt
+}
+
+# grab and process county line from $fco based on FIPS ($4)
+grab ()
+{
+  Pop=$6
+  loc="\"$2,$5\""
+  line=`grep ^$4, $fco`
+# echo "line=$line"
+  county $line
+}
+
+header=`head -1 $fco`
+## Bash string field separator
+IFS=,
+count $header
+unset IFS
+i=0
+cd $CTMP
+while read foo ; do
+  IFS=,
+# echo "foo=$foo"
+  grab $foo
+  unset IFS
+  let i=1+$i
+done < $here/factFIPS.csv
+
+# comparing today with 3 days ago
+title="title='$now COVID-19 cases per 100K'"
+echo $GNUPLOT -e "$title" $here/diff_covid.p
+$GNUPLOT -e "$title" $here/diff_covid.p | $MAGICK convert png:- -rotate 90 diff.png
+cd $here
