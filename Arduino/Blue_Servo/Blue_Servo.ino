893cd6b0e38dcc67d1f997d336dcf178c51e398d
Oristo Po <ormpoa@gmail.com>
2021-01-29 09:11:03 -0500

    missing strap[0] updates

--- Arduino/Blue_Servo/Blue_Servo.ino
+++ Arduino/Blue_Servo/Blue_Servo.ino
@@ -16,11 +16,11 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite (LED, HIGH); // turn the LED off by letting pin go HIGH
-  strap[1].write(90);
+  strap[0].write(90);
   strap[1].write(10);
   delay(1500);                // wait 1.5 seconds
   digitalWrite (LED, LOW);  // turn the LED on by pulling pin LOW
-  strap[1].write(10);
+  strap[0].write(10);
   strap[1].write(90);
   delay(3000);               // wait for 3 seconds
 }
