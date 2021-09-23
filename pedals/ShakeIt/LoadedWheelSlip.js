f5b9024f24063a57ffd27ac52ec9c68fcfe98b3b
blekenbleu <blekenbleu@gmail.com>
2021-09-23 14:19:08 -0400

    use wheelslip property, if available

--- pedals/ShakeIt/LoadedWheelSlip.js
+++ pedals/ShakeIt/LoadedWheelSlip.js
@@ -1,5 +1,8 @@
 // left front loaded slip
 var L = $prop('ShakeITBSV3Plugin.Export.proxyL.FrontLeft');
 // apply left front load to conditioned corner slip
-var s = $prop('ShakeITBSV3Plugin.Export.proxyS.FrontLeft');
+var s = $prop('GameRawData.Physics.WheelSlip01') * 80;
+//return s;
+if (null == s)
+  s = $prop('ShakeITBSV3Plugin.Export.proxyS.FrontLeft');
 return s *  L / 100;
