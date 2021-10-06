4572d1a59425b295136876a3dfce1bd2b502806d
blekenbleu <blekenbleu@gmail.com>
2021-10-06 14:34:22 -0400

    completely untested pwmparms.js

--- Arduino/Blue2charServo/pwmparms.js
+++ Arduino/Blue2charServo/pwmparms.js
@@ -1,4 +1,4 @@
-// update per-servo parameters
+// pwmparms.js update per-channel parameters
 var ns = $prop('Settings.ns');		// servo count
 /* other (unused here) settings:
    $prop('Settings.accel_gain')
@@ -14,6 +14,7 @@ var ts = [0];
 var tm = [0];
 var to = [0];
 //return ns;
+
 if (null == root['ts']) {
   for (i = 1; i <= ns; i++) {
      ts[i-1] = $prop('Settings.p'+i+'min');
@@ -33,36 +34,71 @@ var t1 = $prop('Settings.test_one') && 0 < st;
 var pg = $prop('Settings.page');
 var s = $prop('Settings.servo');
 
-if (2 > pg || ! (wisiwyg || t1)) {
- /* undo some temporary test here
-  if (0 < root['temp']) {
-    s = root['temp'];
-    pg = (s>>7);
-    s &= 0x7F;
-    // undo it
-    root['temp'] = 0;
+var temp; // used by undo()
+var tp;
+var ss;
+var str = '';
+var os = '';		// offset string
+
+function undo() { // undo changes
+  var d = 0;  // calculate tension to apply, based on test being done
+
+  if (tp == 4) {	// Arduino is not yet aware of max tension limits
+    tm[ss - 1] = temp;
+    d = $prop('Settings.p'+ss+'max') + $prop('Settings.p'+ss+'off') - $prop('Settings.p'+ss+'min');
   }
-  */
-  return;
+  else if (tp == 2) {
+    to[ss - 1] = temp;
+    // restore Arduino offset 
+    str += String.fromCharCode(0x40 + ss - 1 | (0x40 & temp) >>1, 0x3F & temp);
+    d = $prop('Settings.p'+ss+'off') - $prop('Settings.p'+ss+'min');
+  }
+  else {  // restore min table
+    ts[ss - 1] = temp;
+    if (3 != pg)
+      os += String.fromCharCode(0x5E,ns)+String.fromCharCode.apply(null,ts);
+  }
+  // include data so that Arduino will set servo position with restored offset
+  str += String.fromCharCode(0x40 + ns + ss - 1 | (0x40 & d) >>1, 0x3F & d);
+  root['temp'] = 0;
+};
+
+if (2 > pg || ! (wisiwyg || t1)) { // nothing new to change
+  if (0 < root['temp']) { // temporary test to undo?
+    temp = root['temp'];
+    tp = 7 & (temp>>7);
+    ss = temp & 0x7F;
+    temp = temp >> 10;
+    pg = 2; // force min table update
+    undo();
+    return os+str;
+  }
+  else return;
 }
 
 ts = root['ts'];
 tm = root['tm'];
 to = root['to'];
-var str = '';
-var os = '';		// offset string
 var change = false;	// Only one page at a time
 var i;
 
-if (root['temp'] && t1 && s == (0x7F & root['temp']) && pg == (7 & (root['temp']>>7))) {
-  if (pg == 2 && root['temp'] >> 10 == $prop('Settings.p'+s+'off')
-   || pg == 3 && root['temp'] >> 10 == $prop('Settings.p'+s+'min')
-   || pg == 4 && root['temp'] >> 10 == $prop('Settings.p'+s+'max'))
-    return;  // no change in temporary test_one settings
+if (root['temp']) {
+  temp = root['temp'];
+  tp = 7 & (temp>>7);
+  ss = 0x7F & temp ;
+  temp = temp >> 10; 
+  if (t1 && 1 == st && s == ss && pg == tp) {
+    if ((pg == 2 && temp == $prop('Settings.p'+ss+'off'))
+     || (pg == 3 && temp == $prop('Settings.p'+ss+'min'))
+     || (pg == 4 && temp == $prop('Settings.p'+ss+'max')))
+      return;  // no change in temporary test_one parm
+  // otherwise, fall thru and replace that parm value
+  }
+  else undo();  // restore that parm value; another parm will be changed. 
 }
 
 for (i = 1; i <= ns; i++) {
-  var d = 0;  // calculate tension to apply, based on test being done, in this case min (page 3)
+  var d = 0;  // calculate tension to apply, based on test being done; d = 0 for min (page 3)
 
   if (4 == pg) {		// max
     d = $prop('Settings.p'+i+'max') + $prop('Settings.p'+i+'off') - $prop('Settings.p'+i+'min');
@@ -76,7 +112,7 @@ for (i = 1; i <= ns; i++) {
     if to[i-1] != $prop('Settings.p'+i+'off')) {
       change = true;
       var o = to[i-1] = $prop('Settings.p'+i+'off');
-      os += String.fromCharCode((0x40 + i - i) | (0x40 & o)>>1, 0x3F & o) ;
+      os += String.fromCharCode((0x40 + i - 1) | (0x40 & o)>>1, 0x3F & o);
     }
   }
   else if (3 == pg) {	//min
@@ -91,26 +127,32 @@ for (i = 1; i <= ns; i++) {
     str += t;
 }
 
-if (change && 2 == pg)	// change to min table?
-  str = String.fromCharCode.apply(null,ts) + str; // update min table, then tension
+if (change) {
+  if (3 == pg)	// change to min table?  update min table, then tension
+    str = String.fromCharCode(0x5E,ns)+String.fromCharCode.apply(null,ts) + str;
 
-if ($prop('Settings.wisiwyg')) {
-  if (3 == pg)
-    root['ts'] = ts;
-  else if (4 == pg)
-    root['tm'] = tm;
-  else if (2 == pg) {
-    root['to'] = to;
-}
-else if (change && 2 == st) {
-  // assumption: SimHub is fast enough that no more than 1 change at a time
-  i = s - 1;
-  if (3 == pg)
-    root['ts'][i] = ts[i];
-  else if (4 == pg)
-    root['tm'][i] = tm[i];
-  else if (2 == pg)
-    root['to'][i] = to[i];
+  if ($prop('Settings.wisiwyg')) {	// no change left unsaved!!
+    if (3 == pg)
+      root['ts'] = ts;
+    else if (4 == pg)
+      root['tm'] = tm;
+    else if (2 == pg)
+      root['to'] = to;
+    root['temp'] = 0;
+  }
+  else { // assumption: SimHub is fast enough that no more than 1 change at a time
+    i = s - 1;
+    var p = (3 == pg) ? ts[i] : (4 == pg) ? tm[i] : to[i];
+    if (2 == st) {
+      if (3 == pg)
+        root['ts'][i] = p;
+      else if (4 == pg)
+        root['tm'][i] = p;
+      else root['to'][i] = p;
+      root['temp'] = 0;
+    }
+    else root['temp'] = (p << 10) | (pg << 7) | s);
+  }
 }
 return str;
 
