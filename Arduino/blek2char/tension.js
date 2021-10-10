d47f17355be2aaf5cc102e0f9e7583e5ee93e34b
blekenbleu <blekenbleu@gmail.com>
2021-10-10 19:48:37 -0400

    eliminate tabs

--- Arduino/blek2char/tension.js
+++ Arduino/blek2char/tension.js
@@ -1,50 +1,50 @@
 // servo tensions from G-forces;  parms from sliders
-var ns = $prop('Settings.np');		// PWM count
+var ns = $prop('Settings.np');                // PWM count
 var t3=[[0],[0],[0],[0],[0]];
-var neut = [];			// precalculate run-time parms
-var tmax = [];			
+var neut = [];                                // precalculate run-time parms
+var tmax = [];                        
 for (var i = 0; i < ns; i++) {
-   t3[0][i] = $prop('Settings.neu'+i);	// pg = 2
-   t3[1][i] = $prop('Settings.min'+i);	// pg = 3
-   t3[2][i] = $prop('Settings.max'+i);	// pg = 4
-   t3[3][i] = $prop('Settings.sca'+i);	// pg = 5
+   t3[0][i] = $prop('Settings.neu'+i);        // pg = 2
+   t3[1][i] = $prop('Settings.min'+i);        // pg = 3
+   t3[2][i] = $prop('Settings.max'+i);        // pg = 4
+   t3[3][i] = $prop('Settings.sca'+i);        // pg = 5
    // run-time values
-   tmax[i] = Math.round(t3[2][i] * (100 - t3[1][i]) * 0.018);		// 180 / 10000
+   tmax[i] = Math.round(t3[2][i] * (100 - t3[1][i]) * 0.018);  // 180 / 10000
    neut[i] = Math.round((t3[0][i] * tmax[i] - t3[1][i] * 180) / 100);
 }
 /* other (unused here) settings:
    $prop('Settings.info')
    $prop('Settings.nf')
  */
-if (null == root['t3']) {	// device connect message initialized Arduino minima
-  var ft = [0];			// Set up data and reset IIR filters
+if (null == root['t3']) {              // device connect message initialized 2 Arduino LUTs
+  var ft = [0];                        // Set up data and reset IIR filters
 
   root['t3'] = t3;
   for (i = 1; i < ns; i++)
      ft[i] = 0;
-  root['ft'] = ft;	// IIR
+  root['ft'] = ft;                     // IIR
 }
 //return root['ft'].toString()
 
-var pg = $prop('Settings.page') - 2;		// zero - based
+var pg = $prop('Settings.page') - 2;                // zero - based
 var wysiwyg = $prop('Settings.wysiwyg') && 0 <= pg;
 
-var st = '';					// tension string:  watch this space
-if (wysiwyg) {					// changes enabled?
+var st = '';                                        // tension string:  watch this space
+if (wysiwyg) {                                      // changes enabled?
   var tr = root['t3'];
-  var change = false;				// Only one page at a time
+  var change = false;                               // Only one page at a time
   var i;
 
   for (i = 0; i < ns; i++)
     if (tr[pg][i] != t3[pg][i]) {
-      var d = 0;          // calculate tension to apply for test; d = 0 for min (page 3)
+      var d = 0;                       // calculate tension to apply for test; d = 0 for min (page 3)
      
       change = true;
-      tmax[i] = Math.round(t3[2][i] * (100 - t3[1][i]) * 0.018);	// % of 180 * (100 - min) /100
-      if (2 == pg)			// max
-	d = tmax[i];
-      else if (0 == pg) 		// offset
-	neut[i] = d = Math.round(t3[pg][i] * tmax[i] / 100);
+      tmax[i] = Math.round(t3[2][i] * (100 - t3[1][i]) * 0.018);     // % of 180 * (100 - min) /100
+      if (2 == pg)                                                   // max
+        d = tmax[i];
+      else if (0 == pg)                                              // offset
+        neut[i] = d = Math.round(t3[pg][i] * tmax[i] / 100);
       st += String.fromCharCode(0x40 + i | (0x40 & d)>>1, 0x3F & d); // tension for changed parm
     }
 //  return st;
@@ -52,23 +52,23 @@ if (wysiwyg) {					// changes enabled?
 //return st.length;
 //change = true;
   if (change) {
-    if (1 <= pg) {					// table change?  update table, then tension
-      var t = tmax;					// Update tmax for either 1 or 2 == pg
-      var tp = pg + 4;					// Arduino table to update
+    if (1 <= pg) {                                       // table change?  update table, then tension
+      var t = tmax;                                      // Update tmax for either 1 or 2 == pg
+      var tp = pg + 4;                                   // Arduino table to update
 
       if (2 < pg)
-	t = t3[pg];					// brain-dead table loading
-      else if (1 == pg) {				// min: update both min and max
-	var m = [];
-	for (i = 0; i < ns; i++)
-	  m[i] = Math.round(t3[pg][i] * 1.8);		// percentages to PWM min: 180 / 100
+        t = t3[pg];                                      // brain-dead table loading
+      else if (1 == pg) {                                // min: update both min and max
+        var m = [];
+        for (i = 0; i < ns; i++)
+          m[i] = Math.round(t3[pg][i] * 1.8);            // percentages to PWM min: 180 / 100
 
-	st = String.fromCharCode(0x5F,tp,ns)+String.fromCharCode.apply(null,m)+st;
-	tp = 6;						// also update tmax
+        st = String.fromCharCode(0x5F,tp,ns)+String.fromCharCode.apply(null,m)+st;
+        tp = 6;                                          // also update tmax
       }
       st = String.fromCharCode(0x5F,tp,ns)+String.fromCharCode.apply(null,t)+st;
     }
-    root['t3'][pg] = t3[pg];				// no change left unsaved!!
+    root['t3'][pg] = t3[pg];                             // no change left unsaved!!
   }
 }
 //return st;
@@ -110,33 +110,33 @@ if (0 > sway)
   swayGamma *= -1;
 //return swayGamma + ' ' + sway
 
-var ts = [];					// Assign forces to servos
-ts[0] = 0;					// left shoulder belt (leftSurgeSway)
-ts[1] = 0;					// right shoulder belt (rightSurgeSway)
-ts[2] = surge;					// upper back (neutral 25)
-ts[3] = swayGamma;				// head cushion twist (neutral 50)
+var ts = [];                                      // Assign forces to servos
+ts[0] = 0;                                        // left shoulder belt (leftSurgeSway)
+ts[1] = 0;                                        // right shoulder belt (rightSurgeSway)
+ts[2] = surge;                                    // upper back (neutral 25)
+ts[3] = swayGamma;                                // head cushion twist (neutral 50)
 /*
-ts[4] = sway;					// left side back
-ts[5] = -sway;					// right side back
-ts[5] = surge;					// lower back
-ts[5] = monoSurgeHeave;				// mono lap belt
-ts[5] = sway;					// left thigh
-ts[5] = -sway;					// right thigh
-ts[5] = monoSwayHeave;				// left lap belt
-ts[5] = monoSwayHeave;				// right lap belt
-ts[5] = monoSurgeSway;				// mono shoulder belt
-ts[5] = frontHeave;				// front center seat
-ts[5] = rearHeave;				// rear center seat
+ts[4] = sway;                                     // left side back
+ts[5] = -sway;                                    // right side back
+ts[5] = surge;                                    // lower back
+ts[5] = monoSurgeHeave;                           // mono lap belt
+ts[5] = sway;                                     // left thigh
+ts[5] = -sway;                                    // right thigh
+ts[5] = monoSwayHeave;                            // left lap belt
+ts[5] = monoSwayHeave;                            // right lap belt
+ts[5] = monoSurgeSway;                            // mono shoulder belt
+ts[5] = frontHeave;                               // front center seat
+ts[5] = rearHeave;                                // rear center seat
  */
 //return ts.toString()
 
 var tc = 1 - ($prop('Settings.smooth') * 0.2);
 for (var i = 0; i < ns; i++) {
-  var ft = root['ft'][i];					// Low-pass IIR filter
+  var ft = root['ft'][i];                         // Low-pass IIR filter
 
-  ft += (ts[i] - ft) * tc;					// filtered tension
+  ft += (ts[i] - ft) * tc;                        // filtered tension
 
-  if (i >= 90)							// debugging
+  if (i >= 90)                                    // debugging
     return [i,ns,ft,Math.abs(ft-root['ft'][i]),e].toString()
 
 /* Skip change if current unfiltered tension differs from previous filtered tension
@@ -146,16 +146,16 @@ for (var i = 0; i < ns; i++) {
   if (tension) {
 //  Center forces by (offset)
     ft += neut[i];
-    if (0 > ft) ft = 0;						// below min 
-    else if (ft > 127) ft = 127;				// 7 bit limit limit 
-    st += String.fromCharCode(0x40 | i | ((0x40 & ft)>>1)); 	// set tension msb
-    st += String.fromCharCode(0x3F & ft);			// 6 lsb
+    if (0 > ft) ft = 0;                                         // below min 
+    else if (ft > 127) ft = 127;                                // 7 bit limit limit 
+    st += String.fromCharCode(0x40 | i | ((0x40 & ft)>>1));     // set tension msb
+    st += String.fromCharCode(0x3F & ft);                       // 6 lsb
   }
 }
 
 //return st.length
 if (0 < st.length) {
-  //return st.length;    // 2 * ns
+  //return st.length;
   return st;
 }
 if (990 < st.length) {
