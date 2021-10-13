0ee5ce7dd1941c789d87eb79f63456635cc176b3
blekenbleu <blekenbleu@gmail.com>
2021-10-12 20:04:13 -0400

    disable LUT loads for ASCII echo

--- Arduino/blek2char/connect.js
+++ Arduino/blek2char/connect.js
@@ -1,6 +1,8 @@
 var reset = [0x7F];	// reset Arduino; initialize min[], tmax[]
 reset[1] = 0x5E; reset[2] = np = $prop('Settings.np');	   // active PWM count
 reset[3] = 0x5F; reset[4] = $prop('Settings.info');        // 3 == HEX trace
+if (4 == reset[4])
+  return String.fromCharCode.apply(null,reset);            // avoid LUT clutter
 var l = reset.length;
 reset[l] = 0x5F; reset[l+1] = 5; reset[l+2] = np;          // min LUT header
 l = reset.length;
@@ -16,7 +18,7 @@ for (var i = 0; i < np; i++) {
     var tmax = .01 * $prop('Settings.max'+i) * (180 - min[i]);
     reset[l+i] = Math.round(126 >= tmax ? tmax : 126);     // set maxima
 }
-//return String.fromCharCode.apply(null,reset);
+return String.fromCharCode.apply(null,reset);
 var st = String.fromCharCode.apply(null,reset);
 var s = st.charCodeAt(0);
 for(i=1;i<np;i++)
